87ae0ce4f7b848dd9f99a42fdef889c593aac2ad
Bramheti Patil <103025887+c-40@users.noreply.github.com>
2024-03-26 15:36:30 +0530

    Update BayesianBeliefNetwork.py

--- BayesianBeliefNetwork.py
+++ BayesianBeliefNetwork.py
@@ -50,3 +50,7 @@ p_sprinkler_true = p_rain * p_sprinkler_true_given_rain
 p_sprinkler_true_given_input = p_sprinkler_true / p_rain
 
 print("Probability of Sprinkler=True given the input:", p_sprinkler_true_given_input)
+
+Input:
+Is it raining? (yes/no): yes
+Probability of Sprinkler=True given the input: 0.8000000000000002
