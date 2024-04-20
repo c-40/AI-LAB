78d6afd41fc5f35546f274f874c7300e6c446f87
Bramheti Patil <103025887+c-40@users.noreply.github.com>
2024-04-20 09:36:56 +0530

    Update HillClimbing.py

--- HillClimbing.py
+++ HillClimbing.py
@@ -33,3 +33,40 @@ print("Best Fitness:", best_fitness)
 Output:-
 Best Solution: [0, 0, 0, 0, 1, 1, 1, 0, 1, 1]
 Best Fitness: 5
+
+
+Modified:
+import random
+def objective_function(solution):
+    return sum(solution)
+def generate_neighbor(current_solution):
+    neighbor = current_solution[:]
+    index=random.randint(0,len(neighbor)-1)
+    neighbor[index]=1-neighbor[index]
+
+    return neighbor
+def hill_climbing():
+    current_solution=[random.randint(0,1) for _ in range(10)]
+    current_fitness=objective_function(current_solution)
+    print("Current solution",current_solution)
+    print("Current Fitness",current_fitness)
+    while True:
+        print("---New neighbor----")
+        neighbor = generate_neighbor(current_solution)
+        neighbor_fitness = objective_function(neighbor)
+        print("Neighbor solution", neighbor)
+        print("Neighbor Fitness", neighbor_fitness)
+        if neighbor_fitness >= current_fitness:
+            current_solution = neighbor
+            current_fitness = neighbor_fitness
+        else:
+            break
+    return current_solution, current_fitness
+
+
+
+print('<--------------Hill Climbing--------------->')
+best_solution, best_fitness = hill_climbing()
+print("---Best solution---")
+print("Best Solution:", best_solution)
+print("Best Fitness:", best_fitness)
